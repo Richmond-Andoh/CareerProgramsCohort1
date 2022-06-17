8bb3ba49dff35fde3706f8947bb85b5aa8661acc
Oliver Mensah <olivermensah96@gmail.com>
2022-06-17 12:15:43 +0000

    Array project challenge

---
+++ JavascriptFundamentals/ProjectTwo/script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const STUDENTS = [
+    {
+        name: "John Mintah",
+        class: "BS2"
+    },
+    {
+        name: "Ishmael Latey",
+        class: "BS5"
+    },
+    {
+        name: "Nana Adwoa Owusu Darko",
+        class: "BS2"
+    },
+    {
+        name: "Peter Kwan",
+        class: "BS5"
+    },
+    {
+        name: "Patience Fosu",
+        class: "BS6"
+    }
+]
